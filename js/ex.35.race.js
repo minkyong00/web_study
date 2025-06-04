12b0f3a13bda3d5064fed81e921312e4baf06c6e
minkyong00 <minkyong131@gmail.com>
2025-06-04 23:29:08 +0900

    ex.35.race 문제 풀이 추가

--- js/ex.35.race.js
+++ js/ex.35.race.js
@@ -10,26 +10,44 @@ const ground = document.querySelector('#ground');
 
 let elapsedtime = 0;
 let interval;
+let isRace = false;
+
 
 document.querySelector('#start').addEventListener('click', e => {
+    if(isRace) return; // 이미 경주중이면 다시 클릭안되게 함
+    isRace = true;
 
     interval = setInterval(() => {
-        time.innerHTML = `${elapsedtime = elapsedtime + 50}ms`;
+        time.innerHTML = `소요시간: ${elapsedtime = elapsedtime + 50}ms`;
     }, 50);
 
-    img.style.position = 'absolute';            
-    addEventListener('keyup', e  => {
-        const currLeft = window.getComputedStyle(img).left;
-        const currLeftNum = parseInt(currLeft.substring(0, currLeft.indexOf('px')));
-        
-        const groundWidth = window.getComputedStyle(ground).width;
-        const groundWidthNum = groundWidth.substring(0, groundWidth.length-2);
-        if(e.code === 'Space'){
-            img.style.left = (currLeftNum + 5) + 'px';
-            console.log(groundWidthNum);
-            console.log(currLeftNum);
-        } else if(groundWidthNum < currLeftNum) {
-            clearInterval(interval);
-        }
-    });
+
 });
+
+addEventListener('keyup', e => {
+    const currLeft = window.getComputedStyle(img).left;
+    const currLeftNum = parseInt(currLeft.substring(0, currLeft.indexOf('px')));
+    const groundWidth = window.getComputedStyle(ground).width;
+    const groundWidthNum = parseInt(groundWidth.substring(0, groundWidth.length -2));
+    const imgWidth = parseInt(window.getComputedStyle(img).width);
+    
+    if(!isRace) return; // 경주 중이 아니면 아래 코드 실행안됨
+    
+    if(e.code === 'Space'){
+        const newLeft = currLeftNum + 30;
+        // 말의 왼쪽 위치와 말 이미지 넓이를 더한 값이
+        // 그라운드 넓이보다 클경우
+        if(newLeft + imgWidth >= groundWidthNum){
+            // (그라운드 - 이미지 넓이)와 이미지 왼쪽 넓이를 같게하여
+            // 그라운드 오른쪽 끝에 고정시킴
+            img.style.left = (groundWidthNum - imgWidth) + 'px'; 
+            clearInterval(interval);
+            isRace = false;
+            time.textContent = `결승선 도착! 소요시간: ${elapsedtime}ms`;
+            e.preventDefault(); // 스페이스 이벤트 멈춤
+        } else {
+            img.style.left = newLeft + 'px';
+        }
+    };
+});
+
